d73c02c31c4ec225f32faa9e8ab97caa5c4e6c31
Herman Wong <herman.wong@coalfire.com>
2025-02-03 13:34:31 -0800

    Update minimum versions, add ethereal password, improve sharing.

--- variables.tf
+++ variables.tf
@@ -3,24 +3,6 @@ variable "secrets" {
   description = "Specifies the friendly name of the new secrets to be created as key and an optional value field for descriptions"
 }
 
-variable "length" {
-  type        = number
-  description = "The length of the password to be generated"
-  default     = 15
-}
-
-variable "special" {
-  type        = bool
-  description = "Include special characters in random password string"
-  default     = true
-}
-
-variable "override_special" {
-  type        = string
-  description = "Provide your own list of special characters"
-  default     = "_%@!" #this is the default as it the necessary special characters to prevent issues for PGSQL or other DB services we deploy
-}
-
 variable "kms_key_id" {
   type        = string
   description = "Specifies the ARN or alias of the AWS KMS customer master key (CMK) to be used to encrypt the secret values in the versions stored in this secret."
@@ -31,49 +13,14 @@ variable "path" {
   description = "Path to organize secrets"
 }
 
-variable "min_lower" {
-  type        = number
-  description = "Minimum number of lower case characters"
-  default     = 1
-}
-
-variable "min_upper" {
-  type        = number
-  description = "Minimum number of upper case characters"
-  default     = 1
-}
-
-variable "min_numeric" {
-  type        = number
-  description = "Minimum number of numeric characters"
-  default     = 1
-}
-
-variable "min_special" {
-  type        = number
-  description = "Minimum number of special characters"
-  default     = 1
-}
-
 variable "partition" {
   type        = string
   description = "The AWS partition to use"
 }
 
-variable "cross_account_ids" {
-  type        = list(string)
-  description = "A list of strings containing the account IDs of AWS accounts that should have cross-account access to this secret"
-  default     = null
-}
-
 variable "empty_value" {
   type        = bool
   description = "Whether the secret should be generated without a value"
-  default     = false
-}
-variable "shared" {
-  type        = bool
-  description = "Whether secrets should be shared across accounts."
   default     = false
 }
 
@@ -99,4 +46,81 @@ variable "recovery_window_in_days" {
   description = "Number of days that AWS Secrets Manager waits before it can delete the secret."
   type        = number
   default     = 30
+}
+
+# Sharing
+variable "shared" {
+  type        = bool
+  description = "Whether secrets should be shared across accounts."
+  default     = false
+}
+
+variable "cross_account_ids" {
+  type        = list(string)
+  description = "A list of strings containing the account IDs of AWS accounts that should have cross-account access to this secret"
+  default     = []
+}
+
+variable "organization_ids" {
+  type        = list(string)
+  description = "The AWS Organization ID to share secrets with. If specified, cross_account_ids will be ignored"
+  default     = []
+}
+
+variable "replicas" {
+  type = list(object({
+    region = string
+    kms_key_arn = optional(string)
+  }))
+  description = "List of regions to replicate the secret to. Each replica can optionally specify a KMS key"
+  default     = []
+}
+
+# Random Password
+variable "exclude_characters" {
+  description = "String of the characters that you don't want in the password"
+  type        = string
+  default     = "\" # $ % & ' ( ) * + , - . / : ; < = > ? [ \\ ] ^ ` { | } ~" # Include some punctuations, but avoid ones known to break database passwords
+}
+
+variable "exclude_lowercase" {
+  description = "Specifies whether to exclude lowercase letters from the password"
+  type        = bool
+  default     = false
+}
+
+variable "exclude_numbers" {
+  description = "Specifies whether to exclude numbers from the password"
+  type        = bool
+  default     = false
+}
+
+variable "exclude_punctuation" {
+  description = "Specifies whether to exclude punctuation characters from the password: ! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~"
+  type        = bool
+  default     = false
+}
+
+variable "exclude_uppercase" {
+  description = "Specifies whether to exclude uppercase letters from the password"
+  type        = bool
+  default     = false
+}
+
+variable "include_space" {
+  description = "Specifies whether to include the space character"
+  type        = bool
+  default     = false
+}
+
+variable "password_length" {
+  description = "Length of the password"
+  type        = number
+  default     = 15
+}
+
+variable "require_each_included_type" {
+  description = "Specifies whether to include at least one upper and lowercase letter, one number, and one punctuation"
+  type        = bool
+  default     = true
 }
