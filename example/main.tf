beb3ce72c4c0eb623a6c4cca5d79a0f259abd92d
mhersom <madison.hersom@coalfire.com>
2025-11-19 10:28:17 -0500

    version update to align with reality

--- example/main.tf
+++ example/main.tf
@@ -8,12 +8,12 @@ module "credentials" {
     aws = aws.mgmt
   }
 
-  kms_key_id = data.terraform_remote_state.account-setup.outputs.sm_kms_key_arn 
+  kms_key_id = data.terraform_remote_state.account-setup.outputs.sm_kms_key_arn
 
   secrets = [
     {
-      secret_name        = "secret_name"                    # Update
-      secret_description = "Creating test credentials."     # Update
+      secret_name        = "secret_name"                # Update
+      secret_description = "Creating test credentials." # Update
     }
   ]
 
@@ -26,7 +26,7 @@ module "credentials" {
   password_length = 20
 
   # Sharing
-  shared = false                                                        # Update to true and select utilize one of the options below if sharing
+  shared = false # Update to true and select utilize one of the options below if sharing
   #organization_ids  = [data.aws_organizations_organization.current.id] # Share with Organizations
   #cross_account_ids = [local.root_account_id]                          # Share across AWS Accounts, update local to appropriate account ID
 }
